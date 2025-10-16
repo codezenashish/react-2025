2dd639a8fc6b1f75fc962c12e136bbc5ed9b50d4
codezenashish <ixtashish@gmail.com>
2025-10-16 11:03:51 +0530

    adding propes folder

---
+++ 03_propes/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Card from './components/Card'
+
+
+function App() {
+  return (
+    <>
+    <Card userName='Ashish Choudhary' />
+    </>
+  )
+}
+
+export default App
